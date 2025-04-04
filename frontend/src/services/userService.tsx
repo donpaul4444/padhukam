f6e4eb4224c84c07a1f74bda4c7cff7b8761d1c5
donpaul4444 <donpaul4444@gmail.com>
2025-04-04 14:08:26 +0530

    API_URL_added

--- frontend/src/services/userService.tsx
+++ frontend/src/services/userService.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = "http://localhost:5000/api/user";
+const API_URI = import.meta.env.VITE_API_URL
 
 export const loginUser = async (email: string, password: string) => {
   try {
